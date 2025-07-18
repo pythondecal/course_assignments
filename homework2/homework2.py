1a96fda9f3bf8b962764f5b9a1d8f33a4f32211d
Brianna Peck <bpeck@sub64-140.uhh.hawaii.edu>
2025-07-18 09:01:49 -1000

    adding homework2/

---
+++ homework2/homework2.py
@@ -0,0 +1,44 @@
+# Make sure this file is located in:
+# python_decal_fa25/yourname/homework2/
+
+# Please answer the following questions as comments.
+# You may have to do some of your own research.
+
+# 1) What is the difference between Git, GitHub and GitBash?
+
+# 2) What is the difference between the terminal and the command line?
+
+# 3) What is the difference between the Windows PowerShell and Git Bash?
+
+# 4) What is the difference between Anaconda, conda and Python?
+
+# 5) What is VS Code? 
+
+# 6) What is a Jupyter Notebook? What is Jupyter Lab?
+
+# 7) What does ~/ mean?
+
+# 8) What is the difference between an absolute versus a relative path?
+
+# 9) Pretend you are in your "yourname" repository. Write the absolute and then the relative path to course_assignments/homework2.
+
+# 10) What command would let you move from course_assignments/homework2/ to course_assignments?
+
+# 11) If you called rm ./ in your current working directory, what would it do? (please don't do this)
+
+# 12) Explain what the following commands do:
+# git add
+# git commit
+# git push
+
+# 13) What's the difference between "git add ." and "git add <file>"?
+
+# 14) What does "git status" do? What does "git log -1" do? 
+
+# 15) What is the difference between cloning a repository and pulling from a repository?
+
+# 16) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+
+# 17) What is a question you have? Or what is a concept you are confused about?
+
+# 18) Tell me a fun fact!
